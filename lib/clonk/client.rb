368e385624da10c90a3853875bf3c082df71f76b
Simon Fish <sfish@ukcloud.com>
2018-12-13 14:37:54 +0000

    Move methods out of client model

--- lib/clonk/client.rb
+++ lib/clonk/client.rb
@@ -12,54 +12,5 @@ module Clonk
       @id = clients_response['id']
       @name = clients_response['clientId']
     end
-
-    ##
-    # Maps the given role into the scope of the client. If a user has that role,
-    # it will be visible in tokens given by this client during authentication.
-    # FIXME: Move to connection class
-
-    def map_scope(client: nil, role: nil, realm: REALM)
-      Clonk.parsed_response(
-        method: :post,
-        data: [role.config],
-        path: "#{url}/scope-mappings/clients/#{client.id}"
-      )
-    end
-
-    ##
-    # Lists the client's permission IDs, if permissions are enabled.
-    # These will be returned as either a boolean (false) if disabled,
-    # or a hash of permission types and IDs.
-    # FIXME: Move to connection class
-
-    def permissions
-      Clonk.parsed_response(
-        path: "#{url}/management/permissions"
-      )['scopePermissions'] || false
-    end
-
-    ##
-    # Enables or disables permissions for a client
-    # FIXME: Move to connection class
-
-    def set_permissions(enabled: true)
-      Clonk.parsed_response(
-        method: :put,
-        path: "#{url}/management/permissions",
-        data: {
-          enabled: enabled
-        }
-      )
-    end
-
-    ##
-    # Returns the client's secret
-    # FIXME: Move to connection class
-
-    def secret
-      Clonk.parsed_response(
-        path: "#{url}/client-secret"
-      )['value']
-    end
   end
 end
--- lib/clonk/connection.rb
+++ lib/clonk/connection.rb
@@ -59,6 +59,57 @@ module Clonk
     end
 
     ##
+    # Maps the given role into the scope of the client. If a user has that role,
+    # it will be visible in tokens given by this client during authentication.
+    # FIXME: Write test!
+
+    def map_scope(client:, role:)
+      response(
+        method: :post,
+        data: [config(role)],
+        path: "#{url_for(client)}/scope-mappings/clients/#{role.container_id}"
+      )
+    end
+
+    ##
+    # Lists the client's permission IDs, if permissions are enabled.
+    # These will be returned as either a boolean (false) if disabled,
+    # or a hash of permission types and IDs.
+    # FIXME: Move to RHSSO so that permissions can actually be used!
+    # FIXME: Write test!
+
+    def permissions(client:)
+      parsed_response(
+        path: "#{url_for(client)}/management/permissions"
+      )['scopePermissions'] || false
+    end
+
+    ##
+    # Enables or disables permissions for a client
+    # FIXME: Write test!
+
+    def set_permissions(client:, enabled: true)
+      parsed_response(
+        method: :put,
+        path: "#{url_for(client)}/management/permissions",
+        data: {
+          enabled: enabled
+        }
+      )
+    end
+
+
+    ##
+    # Returns the client's secret
+    # FIXME: Write test!
+
+    def secret(client:)
+      parsed_response(
+        path: "#{url_for(client)}/client-secret"
+      )['value']
+    end
+
+    ##
     # Creates a role within the given client.
     # it will be visible in tokens given by this client during authentication,
     # as it is already in scope.
